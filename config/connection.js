8aedc1d7f66f7680ae65719f816c37dfc4eb291a
Arslan Mirza <arslantahir0553@gmail.com>
2023-10-06 17:08:53 +0500

    first commit

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const url = "mongodb+srv://mirzaarri:Arri14321@cluster0.qtq9cdd.mongodb.net/"
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(()=>{
+  console.log("Connected");
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console,"connection failed"));
+
+module.exports = mongoose;
